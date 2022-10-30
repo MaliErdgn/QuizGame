fc9e2d157555b363f36de295847851727d466db6
MaliErdgn <malierdgnn@gmail.com>
2022-10-30 21:33:11 +0300

    Update dbtemp.py

--- dbtemp.py
+++ dbtemp.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import random
 
+def soruSorma():
+    global şıklar
+    df = pd.read_excel("QuizGame\soru\sorular.xlsx")
+    sorulardf = df.sample(n = 10)
+
+    for i in range(10):
+        soru = sorulardf.iat[i,0]
+        cevap = sorulardf.iat[i,1]
+        yanlışCevap1 = sorulardf.iat[i,2]
+        yanlışCevap2 = sorulardf.iat[i,3]
+        yanlışCevap3 = sorulardf.iat[i,4]
+        şıklar = [cevap,yanlışCevap1,yanlışCevap2,yanlışCevap3]
+        print(soru)
+        soruText()
+
+
 def soruText():
     current = random.choice(şıklar)
     print(f"A){current}",end="  ")
@@ -15,17 +31,4 @@ def soruText():
     print(f"D){current}")
     şıklar.remove(current)
 
-
-df = pd.read_excel("QuizGame\soru\sorular.xlsx")
-sorulardf = df.sample(n = 10)
-
-for i in range(10):
-    soru = sorulardf.iat[i,0]
-    cevap = sorulardf.iat[i,1]
-    yanlışCevap1 = sorulardf.iat[i,2]
-    yanlışCevap2 = sorulardf.iat[i,3]
-    yanlışCevap3 = sorulardf.iat[i,4]
-    şıklar = [cevap,yanlışCevap1,yanlışCevap2,yanlışCevap3]
-    print(soru)
-    soruText()
 
