f7acc8bd28aa4af0ffb2b554f68db7500edf5705
MaliErdgn <malierdgnn@gmail.com>
2022-10-30 21:57:15 +0300

    Update dbtemp.py

--- dbtemp.py
+++ dbtemp.py
@@ -3,21 +3,21 @@ import random
 
 def soruSorma():
     global şıklar
-    df = pd.read_excel("QuizGame\soru\sorular.xlsx")
-    sorulardf = df.sample(n = 10)
+    df = pd.read_excel("QuizGame\soru\sorular.xlsx")       #pull from excel
+    sorulardf = df.sample(n = 10)       #choose 10 random quesitons
 
-    for i in range(10):
+    for i in range(10):  
         soru = sorulardf.iat[i,0]
-        cevap = sorulardf.iat[i,1]
+        cevap = sorulardf.iat[i,1] 
         yanlışCevap1 = sorulardf.iat[i,2]
         yanlışCevap2 = sorulardf.iat[i,3]
-        yanlışCevap3 = sorulardf.iat[i,4]
-        şıklar = [cevap,yanlışCevap1,yanlışCevap2,yanlışCevap3]
-        print(soru)
+        yanlışCevap3 = sorulardf.iat[i,4]   #take the question and the choices from df
+        şıklar = [cevap,yanlışCevap1,yanlışCevap2,yanlışCevap3]  #put choices in a list for randomness
+        print(soru) 
         soruText()
 
 
-def soruText():
+def soruText():   #choose a random choice, print it as a choice and remove said item from list. Do it 4 times
     current = random.choice(şıklar)
     print(f"A){current}",end="  ")
     şıklar.remove(current)
